4996397fa8d7eed549f0fe4f585da2df1e8d481c
Rani Pawar <pawar.rani241996@gmail.com>
2025-08-08 08:32:31 +0530

    Update variables.js

--- basics/variables.js
+++ basics/variables.js
@@ -3,6 +3,12 @@ let accountEmail = "rani@gmail.com";
 var accountPassword = "123456";
 accountCity = "Nagpur";
 
-accountId = 2;
+// accountId = 2;  // not allowed bcz of the constant value
 
-// console.log(accountId); // not allowed bcz of the constant value
+accountEmail = "p@gmail.com";
+accountPassword = 121212;
+accountCity = "Pune";
+
+console.log(accountId); // not allowed bcz of the constant value
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
