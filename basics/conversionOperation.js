6c26a2bac2341bac368c9c97729f9356bf808615
Rani Pawar <pawar.rani241996@gmail.com>
2025-08-09 10:38:59 +0530

    Update conversionOperation.js

--- basics/conversionOperation.js
+++ basics/conversionOperation.js
@@ -6,3 +6,8 @@ console.log(typeof score);
 let valueInNumber = Number(score);
 console.log(typeof valueInNumber);
 console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1
+// Null => 0
