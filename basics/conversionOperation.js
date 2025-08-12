e250283867b4c58ba3d2414abf147c41f733a8ed
Rani Pawar <pawar.rani241996@gmail.com>
2025-08-12 12:24:55 +0530

    Update conversionOperation.js

--- basics/conversionOperation.js
+++ basics/conversionOperation.js
@@ -1,11 +1,11 @@
 let score = "33abc";
 
-console.log(typeof score);
-console.log(typeof score);
+// console.log(typeof score);
+// console.log(typeof score);
 
 let valueInNumber = Number(score);
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 // "33" => 33
 // "33abc" => NaN
@@ -24,7 +24,7 @@ let booleanIsLoggedIn = Boolean(isLoggedIn);
 let someNumber = 33;
 
 let stringNum = String(someNumber);
-console.log(stringNum);
-console.log(typeof stringNum);
+// console.log(stringNum);
+// console.log(typeof stringNum);
 
 // ****************** OPERATIONS *********************
