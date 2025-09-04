940226f0c94f333fe7f58ad82f74e09517a12556
Rani Pawar <pawar.rani241996@gmail.com>
2025-09-04 10:11:07 +0530

    Update datatypes.summary.js

--- basics/datatypes.summary.js
+++ basics/datatypes.summary.js
@@ -5,3 +5,5 @@
 // REFERENCE TYPE / NON-PRIMITIVE
 
 // Array , Objects, Functions
+
+// SO JAVASCRIPT IS A DYNAMICALY LANGAUGE IT MEANS the type of a variable is determined at runtime, and you can reassign it to a different type later:
