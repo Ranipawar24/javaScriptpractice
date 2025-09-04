12c3601d1a01733d04917d97e034e6f9634d5394
Rani Pawar <pawar.rani241996@gmail.com>
2025-09-04 10:28:28 +0530

    Update datatypes.summary.js

--- basics/datatypes.summary.js
+++ basics/datatypes.summary.js
@@ -2,10 +2,6 @@
 
 // 7 types : string , Number, Boolean, Undefined, Null, Symbol, BigInt
 
-// REFERENCE TYPE / NON-PRIMITIVE
-
-// Array , Objects, Functions
-
 // SO JAVASCRIPT IS A DYNAMICALY LANGAUGE IT MEANS the type of a variable is determined at runtime, and you can reassign it to a different type later:
 
 const score = 100;
@@ -19,3 +15,18 @@ let userEmail; // like its define as a undefined value
 const id = Symbol("123");
 const anotherId = Symbol("123");
 console.log(id === anotherId);
+
+// REFERENCE TYPE / NON-PRIMITIVE
+
+// Array , Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"]; //
+
+let myObj = {
+  name: "Rani",
+  age: 26,
+};
+
+const myFunction = function () {
+  console.log("Hello world");
+};
