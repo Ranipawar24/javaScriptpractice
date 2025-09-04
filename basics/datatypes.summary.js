25e26316533f27c55876fe89bbeae316c411c9d4
Rani Pawar <pawar.rani241996@gmail.com>
2025-09-04 10:15:11 +0530

    Update datatypes.summary.js

--- basics/datatypes.summary.js
+++ basics/datatypes.summary.js
@@ -7,3 +7,11 @@
 // Array , Objects, Functions
 
 // SO JAVASCRIPT IS A DYNAMICALY LANGAUGE IT MEANS the type of a variable is determined at runtime, and you can reassign it to a different type later:
+
+const score = 100;
+const scoreValue = 200.1;
+
+const siLoggedIn = false;
+const outsideTem = null;
+
+let userEmail; // like its define as a undefined value
